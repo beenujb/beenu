ab5d674accf58dffef3663aa19159fe241c1706e
beenujb <41043473+beenujb@users.noreply.github.com>
2018-12-14 14:52:32 +0530

    Update consonant.py

--- consonant.py
+++ consonant.py
@@ -1,4 +1,4 @@
-char = input("Enter a character: ")
+char = input("Enter the character: ")
 if(char=='A' or char=='a' or char=='E' or char =='e' or char=='I'
  or char=='i' or char=='O' or char=='o' or char=='U' or char=='u'):
     print(char, "is a Vowel")
