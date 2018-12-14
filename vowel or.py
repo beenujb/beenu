a3579206b2ae4238bb28769c621021726546150f
beenujb <41043473+beenujb@users.noreply.github.com>
2018-12-14 14:30:58 +0530

    Delete vowel or.py

--- vowel or.py
+++
@@ -1,7 +0,0 @@
-ch = input(" Enter the Character : ")
-
-if(ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u' or ch == 'A'
-       or ch == 'E' or ch == 'I' or ch == 'O' or ch == 'U'):
-    print("The Given Character ", ch, "is a Vowel")
-else:
-    print("The Given Character ", ch, "is a Consonant")
